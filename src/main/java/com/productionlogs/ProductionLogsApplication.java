7ab0adc3b2abe4a14edf14f40494474c827da7e6
Stonewall1 <impression97p@gmail.com>
2022-12-21 11:55:20 +0300

    Initial commit

---
+++ src/main/java/com/productionlogs/ProductionLogsApplication.java
@@ -0,0 +1,13 @@
+package com.productionlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionLogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionLogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/productionlogs/ProductionLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productionlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionLogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
