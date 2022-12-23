769c8eb62202aef3781c0a6f9365e2687837a2f4
Stonewall1 <impression97p@gmail.com>
2022-12-23 14:48:32 +0300

    r1 table edited(sql script) , truncate tables sql file added

---
+++ src/main/resources/db/changelog/db.changelog0-truncate.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--changeset Stonewall: !INSERT ID HERE!
+
+--comment: truncate tables
+
+TRUNCATE TABLE users , r1
--- src/main/resources/db/changelog/db.changelog2-createR1table.sql
+++ src/main/resources/db/changelog/db.changelog2-createR1table.sql
@@ -9,8 +9,5 @@ CREATE TABLE r1
     id               SERIAL PRIMARY KEY,
     operation_start  TIMESTAMP(50),
     operation        varchar(512) NOT NULL,
-    operation_finish TIMESTAMP(50),
-    user_id          BIGINT,
-    CONSTRAINT r1_user_fk
-        FOREIGN KEY (user_id) REFERENCES users (id)
+    operation_finish TIMESTAMP(50)
 );
